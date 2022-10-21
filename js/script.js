05729383e9f42a67d1c20b445633de3f9d8f0eb9
Francesco Foglia <francescofogliajj@gmail.com>
2022-10-21 17:54:11 +0200

    CSS + function startGame(boxesNumber) + function randomNumber(min, max)

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ const start = document.getElementById("start");
 const level = document.getElementsByName("level"); // [Easy, Normal, Hard]
 const game = document.getElementById("game");
 const gameArea = document.getElementById("game-area");
+const bombs = [];
+const points = document.getElementsByClassName("point");
+const bombIcon = "<img src='https://upload.wikimedia.org/wikipedia/commons/9/95/Bomb_icon.svg' class='bomb'></img>"
+const overlays = document.getElementsByClassName("overlay");
 
 
 selectLevel();
@@ -49,6 +53,70 @@ function startGame(boxesNumber) {
 
   createMinesweeper(boxesNumber);
 
+  while (bombs.length < 16) {
+
+    let number = randomNumber(1, boxesNumber);
+
+    if (doubleNumber(number, bombs) == false) {
+      bombs.push(number);
+    }
+
+  }
+
+  for (let i = 0; i < points.length; i++) {
+    points[i] = parseInt(points[i].innerHTML = 1);
+  }
+
+  for (let j = 0; j < points.length; j++) {
+
+    let boxNumbers = j + 1;
+
+    if (doubleNumber(boxNumbers, bombs)) {
+      points[j].innerHTML = bombIcon;
+    }
+
+  }
+
+  let counter = 0;
+
+  let score = document.getElementById("score");
+
+  score.innerHTML = "Score: ";
+
+  for (let i = 0; i < overlays.length; i++) {
+
+    ((index) => {
+
+      overlays[index].addEventListener("click", () => {
+
+        overlays[index].style.opacity = "0";
+
+        overlays[index].style.pointerEvents = "none";
+
+        let clicked = index + 1;
+
+        if (doubleNumber(clicked, bombs)) {
+
+          alert("Game Over");
+          // alert(`Score ${counter}/${max - 16}`);
+
+          let box = document.getElementById("game-grid");
+
+          box.style.pointerEvents = "none";
+
+        } else {
+          counter++;
+        }
+
+        // score.innerHTML = `Score: ${counter}`;
+        score.innerHTML = `Score: ${counter}/${boxesNumber - 16}`;
+
+      })
+
+    })(i);
+
+  }
+
 }
 
 function createMinesweeper(boxesNumber) {
@@ -69,4 +137,24 @@ function createMinesweeper(boxesNumber) {
 
   gameGrid.innerHTML += boxes;
 
+}
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function doubleNumber(element, array) {
+
+  let found = false;
+
+  for (i = 0; i < array.length; i++) {
+
+    if (element == array[i]) {
+      found = true;
+      return found;
+    }
+
+  }
+
+  return found;
 }
